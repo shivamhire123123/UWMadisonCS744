fa27f0c4cb3ac791ffa5b354d9afd1b318b80ea2
Shivam Hire <hire@wisc.edu>
2024-02-02 12:39:22 -0600

    improved local.sh

--- cloudlabnode.sh
+++ cloudlabnode.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install openjdk-8-jdk
+sudo apt install openjdk-8-jdk -y
 sudo mkfs.ext4 /dev/xvda4
 sudo mkdir -p /mnt/data
 sudo mount /dev/xvda4 /mnt/data
--- local.sh
+++ local.sh
@@ -1,19 +1,40 @@
 source nodes.sh
 
 #add ssh keys to node
-#scp id_ed25519* $node0:~/.ssh/.
-#scp id_ed25519* $node1:~/.ssh/.
-#scp id_ed25519* $node2:~/.ssh/.
+scp id_ed25519* $node0:~/.ssh/.
+scp id_ed25519* $node1:~/.ssh/.
+scp id_ed25519* $node2:~/.ssh/.
 
 scp ssh_config $node0:~/.ssh/config
 scp ssh_config $node1:~/.ssh/config
 scp ssh_config $node2:~/.ssh/config
 
 #clone project repo
-ssh $node0 git clone git@github.com:shivamhire123123/UWMadisonCS744.git
-ssh $node1 git clone git@github.com:shivamhire123123/UWMadisonCS744.git
-ssh $node2 git clone git@github.com:shivamhire123123/UWMadisonCS744.git
+ssh $node0 << EOF
+	if [ -d "UWMadisonCS744" ]; then
+		cd UWMadisonCS744
+		git pull
+	else
+		git clone git@github.com:shivamhire123123/UWMadisonCS744.git
+	fi
+EOF
+ssh $node1 << EOF
+	if [ -d "UWMadisonCS744" ]; then
+		cd UWMadisonCS744
+		git pull
+	else
+		git clone git@github.com:shivamhire123123/UWMadisonCS744.git
+	fi
+EOF
+ssh $node2 << EOF
+	if [ -d "UWMadisonCS744" ]; then
+		cd UWMadisonCS744
+		git pull
+	else
+		git clone git@github.com:shivamhire123123/UWMadisonCS744.git
+	fi
+EOF
 
-#ssh $node0 ~/UWMadisonCS744/cloudlabnode.sh
-#ssh $node1 ~/UWMadisonCS744/cloudlabnode.sh
-#ssh $node2 ~/UWMadisonCS744/cloudlabnode.sh
+#ssh $node0 bash UWMadisonCS744/cloudlabnode.sh
+#ssh $node1 bash UWMadisonCS744/cloudlabnode.sh
+#ssh $node2 bash UWMadisonCS744/cloudlabnode.sh
