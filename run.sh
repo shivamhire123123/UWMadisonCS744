6cb7a3712794598585d54e30a93924e2db226ac8
Srihari S <43612273+Srihari123456@users.noreply.github.com>
2024-02-08 17:54:48 +0000

    Removed redundant files

--- run.sh
+++
@@ -1,6 +0,0 @@
-# Run a Python application on a Spark standalone cluster
-/mnt/data/spark-3.3.4-bin-hadoop3/bin/spark-submit \
-  --master spark://10.10.1.1:7077 \
-  /mnt/data/iot_details/part_2.py \
-  --input /mnt/data/iot_details/export.csv \
-  --output /mnt/data/iot_details/result
