97079179810db32c32480ca39d80e67d674fe039
Anshuman Senapati <60324361+Anshu1245@users.noreply.github.com>
2024-02-08 12:54:10 +0530

    Update run_page_rank.sh

--- run_page_rank.sh
+++ run_page_rank.sh
@@ -3,6 +3,6 @@ hdfs dfs -rm -r -f hdfs:///wiki_pagerank_results.txt
 time /mnt/data/spark-3.3.4-bin-hadoop3/bin/spark-submit \
   --master spark://node0.cs744s24-g22.uwmadison744-s24-pg0.wisc.cloudlab.us:7077 \
   /mnt/data/page_rank/xmlPageRank.py \
-  --input hdfs://10.10.1.1:9000/web-BerkStan.txt \
-  --output hdfs://10.10.1.1:9000/result1 \
+  --input hdfs://10.10.1.1:9000/concatenated_output.xml \
+  --output hdfs://10.10.1.1:9000/wiki_pagerank_results.txt \
   --partitions 4
